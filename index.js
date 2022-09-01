ded5eff2824d4499aaeee297217f6bf7ef3e74b3
coconutmacaroon <45187468+CoconutMacaroon@users.noreply.github.com>
2022-08-31 21:23:44 -0700

    Basic code

---
+++ index.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+// TODO: for mac support, look into this:
+// https://www.electronjs.org/docs/latest/tutorial/quick-start#open-a-window-if-none-are-open-macos
