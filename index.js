00cbc835ae7285c49a6df56247c008b9a5c48a52
coconutmacaroon <45187468+CoconutMacaroon@users.noreply.github.com>
2022-09-04 18:49:14 -0700

    Formatting works

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
-    win.loadFile('index.html')
-}
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+
+    // this won't work on Macs
+    win.setMenuBarVisibility(false);
+    win.loadFile("index.html");
+};
 
 app.whenReady().then(() => {
-    createWindow()
-})
+    createWindow();
+});
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
 
 // TODO: for mac support, look into this:
 // https://www.electronjs.org/docs/latest/tutorial/quick-start#open-a-window-if-none-are-open-macos
